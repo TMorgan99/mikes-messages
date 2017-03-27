7fe361ed6ed8c84657e6d94e3e26f1c3de8233ad
tmorgan99 <tmorgan99@gmail.com>
2017-03-27 21:25:49 +0000

    signin with extended fields

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
   resources :posts
-  devise_for :users
+  # devise_for :users
+  devise_for :users, :controllers => { registrations: 'my_registrations' }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
